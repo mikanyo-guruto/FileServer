65570273e27adadb9092bd98422a585146b4d7b5
Miura <you@example.com>
2017-11-21 14:55:28 +0900

    test

---
+++ admin/db_config.php
@@ -0,0 +1,15 @@
+<?php
+
+	// DB接続設定をreturnする
+	function db_config() {
+		//
+		return array(
+			'user' => 'mikanyo',
+			'database' => 'mikanyo',
+			'pass' => 'mikanyo',
+			'host' => 'localhost',
+			'charset' => 'utf8mb4',
+		);
+	}
+
+	$up_dir = "http://dev2.m-fr.net/keisuke07/fileserver/files/";
---
+++ admin/dbh.php
@@ -0,0 +1,38 @@
+<?php
+	require_once('db_config.php'); // DBの設定ファイル
+
+	// DB用関数
+	// -------------------------------------------------------
+	function get_dbh() {
+		// 「二重接続」を防ぐロジック
+		static $dbh = NULL;
+		if (NULL !== $dbh) {
+			return $dbh;
+		}
+
+		// 設定値の取得
+		$db_config = db_config();
+		// データの設定
+		$user = $db_config['user'];
+		$pass = $db_config['pass'];
+		$dsn = "mysql:dbname={$db_config['database']};host={$db_config['host']};charset={$db_config['charset']}";
+
+		// 接続オプションの設定
+		$opt = array (
+			PDO::ATTR_EMULATE_PREPARES => false,
+		);
+		// 「複文禁止」が可能なら付け足しておく
+		if (defined('PDO::MYSQL_ATTR_MULTI_STATEMENTS')) {
+			$opt[PDO::MYSQL_ATTR_MULTI_STATEMENTS] = false;
+		}
+
+		// 接続
+		try {
+			$dbh = new PDO($dsn, $user, $pass, $opt);
+		} catch (PDOException $e) {
+			echo 'エラーが起きました';
+			exit;
+		}
+		//
+		return $dbh;
+	}
